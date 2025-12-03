963a34c33591932d8a7af08aea926eb2d80ba367
Hoi Tong Yeung <hoitonghy1213@gmail.com>
2025-12-03 23:59:24 +0000

    major change

--- hollow_triangle.sh
+++ hollow_triangle.sh
@@ -1,1 +1,44 @@
 #!/bin/sh
+
+if [ $# -eq 0 ] || [ $# -gt 1 ]
+then
+    echo "Usage: hollow_triangle.sh ${1}"
+    exit 1
+elif [ ${1} -le 0 ]
+then
+    echo "Error: height must be a positive integer"
+    exit 1
+else
+
+    count_out=1
+    while [ $count_out -le "${1}" ]
+    do
+        if [ $count_out -eq 1 ]
+        then  
+            echo "*"
+        
+        elif [ $count_out -eq "${1}" ]
+        then
+            last_line=1
+            while [ $last_line -le "${1}" ]
+            do
+                echo -n "*"
+                last_line=$((last_line+1))
+            done
+            echo ""
+        else
+            
+            echo -n "*"
+            count_in=3
+            while [ $count_in -le $count_out ]
+            do
+                echo -n " "
+                count_in=$((count_in+1))
+            done
+
+            echo "*"
+        fi
+        count_out=$(( count_out+1 ))
+    done
+    exit 0
+fi
--- main.c
+++ main.c
@@ -3,12 +3,54 @@
 
 int main(void) {
     /* ───────── strlen checks ───────── */
-
+    const char *str = "abc";
+    assert(strlen(str)==3);
+    
+    assert(strlen(NULL) == 0);
+    assert(strlen("")==0);
+    assert(strlen(" ")== 1
+)
     /* ───────── strstr checks ───────── */
+    assert(strstr("abc", "a")== 0);
+    assert(strstr("abc", "d")==NULL);
+    assert(strstr(NULL, "a")== NULL);
+    assert(strstr(NULL, NULL)==NULL);
+    assertEqual(strstr("abc", NULL)=="abc");
 
     /* ───────── strncpy checks ───────── */
+    char buffer1[5]; /*when src is less than n*/
+    strncpy(buffer1, "ab", 5);
+    assertEqual(buffer1, "ab\0\0");
+
+    char buffer2[5]; /*when src is equal to n*/
+    strncpy(buffer2, "abcde", 5);
+    assertEqual(buffer2, "abcde");
+
+    char buffer3[11]; /*when src is longer than n*/
+    strncpy(buffer3, "abcdefghijk", 5);
+    assertEqual(buffer3, "abcdefghijk");
+
+    char buffer4[5]; /*when src is NULL*/
+    strncpy(buffer4, NULL, 4);
+    assertEqual(buffer4, "\0\0\0\0");
+
+    char buffer5[1]; /*when n is 0 and src is NULL*/
+    strncpy(buffer5, NULL, 0);
+    assertEqual(buffer5, "");
 
     /* ───────── strcmp checks ───────── */
+    assert(strcmp("abc", "abc")==0);
+    assert(strcmp("", "")==0);
+
+    assert(strcmp(NULL, NULL)==0);
+
+    assert(strcmp("ab", "yz") < 0);
+    assert(strcmp("abc", "abz") < 0);
+    assert(strcmp("", "a") < 0);
+
+    assert(strcmp("yz", "ab") > 0);
+    assert(strcmp("abz", "abc") > 0);
+    assert(strcmp("a", "") > 0);
 
     return 0;
 }
